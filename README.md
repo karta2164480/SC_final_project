bc399fd6a6a41fe811b8976219a5bf251bcb2547
karta2164480 <karta2164480@gmail.com>
2020-06-07 23:08:00 +0800

    add time sector limit

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 this is my code and score recode for SC final project, i.e., joining in a competition about singing transcription.  
 
 ## score recode 
-0.2268110 by using picth and average spectral flux.  
+0.2325740 by using picth and average spectral flux and time sector limit.  
--- test.m
+++ test.m
@@ -19,37 +19,37 @@ for i = 1:1500
     
     for j = 2:dimension(1)
         
-        if(j<dimension(1)&&(pv.flux(j)+pv.flux(j+1)+pv.flux(j-1))/3 > 0.02 &&flux_flag==0)
-            flux_flag=1;
-        else
-            flux_flag=0;
-        end
+        %if(j<dimension(1)&&(pv.flux(j)+pv.flux(j+1)+pv.flux(j-1))/3 > 0.02 &&flux_flag==0)
+        %    flux_flag=1;
+        %else
+        %    flux_flag=0;
+        %end
         
         if(pv.pitch(j) ~= 0 && flag == 0)
-            result.onset(note_count)=pv.time(j)-0.016;
+            result.onset(note_count)=pv.time(j);
             pitch(note_count,time_sector)=pv.pitch(j);
             time_sector = time_sector+1;
             flag = 1 ;
             %j = j + 1;
         
         elseif(pv.pitch(j) == 0 && flag == 1)
-            result.offset(note_count)=pv.time(j)-0.016;
+            result.offset(note_count)=pv.time(j);
             result.output(note_count)=trimmean(pitch(note_count),40);
             note_count=note_count+1;
             time_sector = 1;
             flag = 0;
         
         
-        elseif((abs(pv.pitch(j)-pv.pitch(j-1)) >= 0.8 || (abs(pv.pitch(j)-pv.pitch(j-1)) >= 0.5&&flux_flag==1))&& flag == 1)
-            result.offset(note_count)=pv.time(j)-0.016;
+        elseif((abs(pv.pitch(j)-pv.pitch(j-1)) >= 0.8 || (j<dimension(1)&&(pv.flux(j)+pv.flux(j+1)+pv.flux(j-1))/3 > 0.04))&& flag == 1 && time_sector>4)
+            result.offset(note_count)=pv.time(j);
             result.output(note_count)=trimmean(pitch(note_count),40);
             note_count=note_count+1;
             time_sector = 1;
-            result.onset(note_count)=pv.time(j)-0.016;
+            result.onset(note_count)=pv.time(j);
             pitch(note_count,time_sector)=pv.pitch(j);
             flux_flag=2;
         
-        elseif(abs(pv.pitch(j)-pv.pitch(j-1))<0.8 && flag == 1)
+        elseif(flag == 1)
             pitch(note_count,time_sector)=pv.pitch(j);
             time_sector=time_sector+1;
         end
